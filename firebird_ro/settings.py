02092868045ac5f0576a65c940dcf76104a56829
Popa Marius Adrian <mapopa@gmail.com>
2015-01-19 12:18:46 +0200

    Update settings.py

--- firebird_ro/settings.py
+++ firebird_ro/settings.py
@@ -121,8 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'firebird_ro.news',
     'django.contrib.admin',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
